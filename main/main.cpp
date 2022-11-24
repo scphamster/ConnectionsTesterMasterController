347548350fe98bbff892f1e69ade5783752cf60e
scphamster <scp.hamster@gmai.com>
2022-11-24 12:56:53 +0300

    constructor implemented

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,27 @@
-#include <stdio.h>
+#include "esp_log.h"
 #include "driver/gpio.h"
-//#include "../components/cpp_wrappers/gpio/include/gpio.hpp"
+#include <stdio.h>
+// #include "../components/cpp_wrappers/gpio/include/gpio.hpp"
 #include "gpio.hpp"
 
-extern "C" void app_main()
+#include "freertos/FreeRTOS.h"
+#include "FreeRTOS/task.h"
+
+extern "C" void
+app_main()
 {
-    IO io;
+    Pin p1{0, Pin::Direction::Input };
+    auto old_lvl = p1.GetLevel();
 
+    while(1) {
+        auto current_lvl = p1.GetLevel();
 
+        if (current_lvl != old_lvl) {
+            old_lvl = current_lvl;
+            ESP_LOGI("DBG", "PinLvl Changed, lvl=%i", static_cast<int>(current_lvl));
+            vTaskDelay(pdMS_TO_TICKS(50));
+        }
 
-
+        vTaskDelay(1);
+    }
 }
